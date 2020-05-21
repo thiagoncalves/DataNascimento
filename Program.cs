76d5c0e0c6eead32f7549463446919f4c4551117
Thiago Gonçalves Novaes <thiago.novaes@bunzlbrasil.protcapad.com.br>
2020-05-21 18:49:25 -0300

    Novo Projeto

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataNascimento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dia;
+            int mes;
+            int ano;
+
+            Console.WriteLine("Digite o dia do seu aniverário: ");
+            dia = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o mês do seu aniverário: ");
+            mes = Convert.ToInt32(Console.ReadLine());
+
+            string[] listames = new string[12];
+            listames[0] = "Janeiro";
+            listames[1] = "Fevereiro";
+            listames[2] = "Março";
+            listames[3] = "Abril";
+            listames[4] = "Maio";
+            listames[5] = "Junho";
+            listames[6] = "Julho";
+            listames[7] = "Agosto";
+            listames[8] = "Setembro";
+            listames[9] = "Outubro";
+            listames[10] = "Novembro";
+            listames[11] = "Dezembro";
+
+            Console.WriteLine("Digite o ano do seu aniverário: ");
+            ano = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.WriteLine("Data de Nascimento: " + dia.ToString("00") + "/" + mes.ToString("00") + "/" + ano);
+            Console.WriteLine("Você nasceu em: " + dia.ToString("00") + " de " + listames[mes -1] + " de " + ano);
+
+            Console.ReadLine();
+        }
+    }
+}
