bc4562d8d38f6f80f2052ae921cb6f878168a0de
Thiago Gonçalves Novaes <thiago.novaes@bunzlbrasil.protcapad.com.br>
2020-05-23 01:28:03 -0300

    Second Version

--- Program.cs
+++ Program.cs
@@ -20,6 +20,21 @@ namespace DataNascimento
             Console.WriteLine("Digite o mês do seu aniverário: ");
             mes = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("Digite o ano do seu aniverário: ");
+            ano = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+            string nomeMes = CapturarMês(mes);
+
+            Console.WriteLine("Data de Nascimento: " + dia.ToString("00") + "/" + mes.ToString("00") + "/" + ano);
+            Console.WriteLine("Você nasceu em: " + dia.ToString("00") + " de " + nomeMes + " de " + ano);
+
+            Console.ReadLine();
+        }
+
+        public static string CapturarMês (int mes)
+        {
             string[] listames = new string[12];
             listames[0] = "Janeiro";
             listames[1] = "Fevereiro";
@@ -34,15 +49,7 @@ namespace DataNascimento
             listames[10] = "Novembro";
             listames[11] = "Dezembro";
 
-            Console.WriteLine("Digite o ano do seu aniverário: ");
-            ano = Convert.ToInt32(Console.ReadLine());
-
-            Console.Clear();
-
-            Console.WriteLine("Data de Nascimento: " + dia.ToString("00") + "/" + mes.ToString("00") + "/" + ano);
-            Console.WriteLine("Você nasceu em: " + dia.ToString("00") + " de " + listames[mes -1] + " de " + ano);
-
-            Console.ReadLine();
+            return listames[mes - 1];
         }
     }
 }
